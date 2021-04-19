bbebe13752f8ab61a966e13829be9b52537b3bbe
Elijah Delos Reyes <elijah@Elijahs-MacBook-Pro.local>
2021-04-18 20:12:28 -0700

    yup

--- src/components/Features.js
+++ src/components/Features.js
@@ -9,6 +9,10 @@ export default class Features extends Component {
                 <h3>Collect your content</h3>
                 <h4>Your favorite content lives across different places of the web like blogs, tweets, vidoes, posts, and podcasts.</h4>
                 <h4>Instead of bookmarking for future reference -you can use our google chrome extension to drag n drop your content to store on your dashboard.</h4>
+                <br/>
+                <h3>Organize your content</h3>
+                <h4>By using our tool- you can set-up your content to create magic later.</h4>
+                <h4>When you find your content  you can add descriptions, give your quick thoughts, add titles, and curate based on your tastes.</h4>
             </div>
         </div>);
     }
