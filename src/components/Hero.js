9083e87f4c93a7add61c96f95375aa720b838aac
Elijah Delos Reyes <delosreyes.elijah@gmail.com>
2021-04-21 20:19:33 -0700

    good for mobile

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,13 +5,15 @@ export default class Hero extends Component {
 		return (
 			<div ref={this.props.setRef}>
 				<div className="hero-container">
-					<div className="hero-div">
-						<h1>
+					<div style={{ paddingRight: '10px' }}>
+						<h1 className="hero-text">
 							A fun way to find new perspectives and build your
 							brand.
 						</h1>
-						<h4 style={{ paddingBottom: '50px' }}>
-							{' '}
+						<h4
+							className="hero-text"
+							style={{ paddingBottom: '50px' }}
+						>
 							Curate your favorite content from the web and
 							organize it as pinboards that you can offer to your
 							community.
@@ -23,7 +25,7 @@ export default class Hero extends Component {
 							Diffuse
 						</button>
 					</div>
-					<div className="hero-image-div">
+					<div>
 						<img className="hero-image" alt=""></img>
 					</div>
 				</div>
